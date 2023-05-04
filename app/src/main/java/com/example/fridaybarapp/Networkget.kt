832c9fc658c565b58d47e331d27d8ee9e6d33dc9
kevinschoenberg <kevin27@outlook.dk>
2023-05-04 14:49:48 +0200

    Added functionality to create list of bars from network response

--- app/src/main/java/com/example/fridaybarapp/Networkget.kt
+++ app/src/main/java/com/example/fridaybarapp/Networkget.kt
@@ -1,32 +1,54 @@
 package com.example.fridaybarapp
 
+import android.media.MediaDrm.LogMessage
+import android.util.Log
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import java.io.IOException
 
-fun makeNetworkRequest(): String? {
-    val client = OkHttpClient()
-    val request = Request.Builder()
-        //.url("https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCcOgjIpA0cnS6OFZJZdHxcZOhjcArLTow&place_id=ChIJg6LFyQE_TEYRueo5XCvJ8ck")
-        .url("https://app.dokkedalleth.dk")
-        .build()
+suspend fun makeNetworkRequest(): String? {
+    return withContext(Dispatchers.IO) {
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url("https://app.dokkedalleth.dk")
+            .build()
 
-    try {
-        val response = client.newCall(request).execute()
-        return response.body?.string()
-    } catch (e: IOException) {
-        e.printStackTrace()
-        return "didnt work"
+        try {
+            val response = client.newCall(request).execute()
+            return@withContext response.body?.string()
+        } catch (e: IOException) {
+            e.printStackTrace()
+            val list = listOf<String>("error")
+            return@withContext list.toString()
+        }
     }
 }
 
-class BarInfo(_name: String, _address: String, _page: String){
-    var name: String = _name;
-    var address: String = _address;
-    var page: String = _page;
+data class BarInfo(val name: String, val address: String, val page: String){
 }
 
-fun createListFromNetworkResponse(response: String): List<BarInfo>{
-    var trimEnd = response.substringBeforeLast("endOfBarList").substringAfter("startOfBarList");
+fun createListFromNetworkResponse(response: String): MutableList<BarInfo>{
+    val returnList: MutableList<BarInfo> = mutableListOf()
+    Log.v("test", "test")
+    val trimEnd = response.substringBeforeLast("endOfBarList").substringAfter("startOfBarList");
 
+    val nameString = trimEnd.substringAfter("name:").substringBefore("address:")
+    val nameList = nameString.split(",_").dropLast(1)
+
+    val addressString = trimEnd.substringAfter("address:").substringBefore("page:")
+    val addressList = addressString.split(",_").dropLast(1)
+
+    val pageString = trimEnd.substringAfter("page:")
+    val pageList = pageString.split(",_").dropLast(1)
+
+    Log.v("test", nameList.size.toString())
+    if(nameList.isNotEmpty()) {
+        for (n in nameList.indices) {
+            returnList.add(BarInfo(nameList[n], addressList[n], pageList[n]))
+        }
+    }
+
+    return returnList;
 }
