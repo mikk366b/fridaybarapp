6ca612ad308daf05b91184256d3891544429f243
AsgerLeth <202004818@post.au.dk>
2023-05-04 15:10:37 +0200

    Implemented HTTP get to get JSON list, and markers on MapScreen.

--- app/src/main/java/com/example/fridaybarapp/MainActivity.kt
+++ app/src/main/java/com/example/fridaybarapp/MainActivity.kt
@@ -20,8 +20,6 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.runtime.*
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
